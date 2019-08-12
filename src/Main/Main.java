511bc290c72ed3afc1bca62e01b9e624d7935c4e
ashu <ashutosh.kuma_cs18>
2019-08-12 15:31:26 +0530

    Main.java: add input statement

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,12 +1,20 @@
 /*
  *@author Ashutosh Kumar
- * version 1.0
+ * versio 1.0
+
  */
 package Main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello,World!");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " X " + i + " = " + (number * i));
+
+        }
 
     }
 
